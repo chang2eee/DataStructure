e38b880d7c4cd60bf49456f2957e654ced7e393b
김병창 <106654981+KimByeongchang@users.noreply.github.com>
2023-03-16 03:13:35 +0900

    Add files via upload

---
+++ Queue/연결구조의 큐.c
@@ -0,0 +1,84 @@
+#include <stdio.h>
+#define MAX 100
+#define true 1
+#define false 0
+int front = -1;
+int rear = -1;
+int queue[MAX];
+
+int IsEmpty(void) {
+    if (front == rear)
+        return true;
+    else return false;
+}
+int IsFull(void) {
+    int tmp = (rear + 1) % MAX;
+    if (tmp == front)
+        return true;
+    else
+        return false;
+}
+void addQ(int value) {
+    if (IsFull())
+        printf("큐가 포화상태이다.\n");
+    else {
+        rear = (rear + 1) % MAX;
+        queue[rear] = value;
+    }
+
+}
+
+int deleteQ() {
+    if (IsEmpty())
+        printf("큐가 비었다.\n");
+    else {
+        front = (front + 1) % MAX;
+        return queue[front];
+    }
+    return 0;
+}
+
+int count() { 
+    return rear - front;
+}
+
+void printQ() {
+    printf("큐 상태\n");
+    for (int i = front + 1; i <= rear; i++) {
+        printf("%d ", queue[i]);
+    }
+    printf("\n");
+}
+
+int main() {
+    int i = 1;
+    char select;
+
+    while (1)
+    {
+        printf("Insert는 A/a, Delete는 L/l Quit은 Q/q 입력\n");
+        scanf("%c", &select);
+
+        if (select == 'A' || select == 'a')
+        {
+            addQ(i);
+            i++;
+            printQ();
+            printf("\n");
+        }
+        else if (select == 'L' || select == 'l')
+        {
+            printf("다음분은 %d번 입니다.\n", deleteQ());
+            printQ();
+            printf("\n");
+        }
+        else if (select == 'Q' || select == 'q')
+            break;
+        getchar();
+    }
+
+    printf("총 대기인원은 %d명 입니다.\n", count());
+    printQ();
+
+    return 0;
+}
